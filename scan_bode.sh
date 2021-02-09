0d608689385586c025327ecd063b3392aff8708f
Francesco Urbani <francescourbanidue@gmail.com>
2021-02-09 14:55:56 +0100

    added scan script.

---
+++ scan_bode.sh
@@ -0,0 +1,2 @@
+# requires imagemagick
+convert -density 150 -colorspace gray bode_template.pdf -linear-stretch 3.5%x10% -blur 0x0.1 -attenuate 0.4 +noise Gaussian -rotate 0.6 bode_template_that_looks_handwritten.pdf
